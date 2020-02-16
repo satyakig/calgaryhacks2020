cbf6f0f99b32f4688194d278944a7ba204ad97d1
ac1214 <ac1214@users.noreply.github.comgit config --global user.email ac1214@users.noreply.github.com>
2020-02-15 22:35:40 -0700

    working with errors

--- frontend/src/components/Tasks/Tasks.jsx
+++ frontend/src/components/Tasks/Tasks.jsx
@@ -1,55 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 import Checkbox from 'components/CustomCheckbox/CustomCheckbox.jsx';
 import Button from 'components/CustomButton/CustomButton.jsx';
+import { useSelector } from 'react-redux';
 
-export class Tasks extends Component {
-  handleCheckbox = (event) => {
+export const Tasks = () => {
+  const courses = useSelector((state) => {
+    return state.userReducer.selectedCourses;
+  });
+  const [day, setDay] = useState([]);
+
+  function getstudyplan(courses) {
+    fetch(`https://calgaryhacks2020.appspot.com/getstudyplan/${courses.join(',')}`)
+      .then((res) => res.json())
+      .then((result) => {
+        var now = new Date();
+        var start = new Date(now.getFullYear(), 0, 0);
+        var diff = now - start;
+        var oneDay = 1000 * 60 * 60 * 24;
+        var day = Math.floor(diff / oneDay);
+
+        setDay(result['todo_list'][day]);
+      });
+
+    console.log(`https://calgaryhacks2020.appspot.com/getstudyplan/${courses.join(',')}`);
+  }
+
+  useEffect(() => {
+    getstudyplan(courses);
+  }, [courses]);
+
+  function handleCheckbox(event) {
     const target = event.target;
     console.log(event.target);
     this.setState({
       [target.name]: target.checked,
     });
-  };
-  render() {
-    const edit = <Tooltip id="edit_tooltip">Edit Task</Tooltip>;
-    const remove = <Tooltip id="remove_tooltip">Remove</Tooltip>;
-    const tasksTitle = [
-      'Sign contract for "What are conference organizers afraid of?"',
-      'Lines From Great Russian Literature? Or E-mails From My Boss?',
-      'Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroi',
-      'Create 4 Invisible User Experiences you Never Knew About',
-      'Read "Following makes Medium better"',
-      'Unfollow 5 enemies from twitter',
-    ];
-    const tasks = [];
-    let number;
-    for (let i = 0; i < tasksTitle.length; i++) {
-      number = `checkbox${i}`;
-      tasks.push(
-        <tr key={i}>
-          <td>
-            <Checkbox number={number} isChecked={i === 1 || i === 2} />
-          </td>
-          <td>{tasksTitle[i]}</td>
-          <td className="td-actions text-right">
-            <OverlayTrigger placement="top" overlay={edit}>
-              <Button bsStyle="info" simple type="button" bsSize="xs">
-                <i className="fa fa-edit" />
-              </Button>
-            </OverlayTrigger>
-
-            <OverlayTrigger placement="top" overlay={remove}>
-              <Button bsStyle="danger" simple type="button" bsSize="xs">
-                <i className="fa fa-times" />
-              </Button>
-            </OverlayTrigger>
-          </td>
-        </tr>,
-      );
-    }
-    return <tbody>{tasks}</tbody>;
   }
-}
+
+  const edit = <Tooltip id="edit_tooltip">Edit Task</Tooltip>;
+  const remove = <Tooltip id="remove_tooltip">Remove</Tooltip>;
+  const tasksTitle = day;
+  console.log(day);
+
+  const tasks = [];
+  let number;
+  for (let i = 0; i < tasksTitle.length; i++) {
+    number = `checkbox${i}`;
+    tasks.push(
+      <tr key={i}>
+        <td>
+          <Checkbox number={number} isChecked={i === 1 || i === 2} />
+        </td>
+        <td>{tasksTitle[i]}</td>
+        <td className="td-actions text-right">
+          <OverlayTrigger placement="top" overlay={edit}>
+            <Button bsStyle="info" simple type="button" bsSize="xs">
+              <i className="fa fa-edit" />
+            </Button>
+          </OverlayTrigger>
+
+          <OverlayTrigger placement="top" overlay={remove}>
+            <Button bsStyle="danger" simple type="button" bsSize="xs">
+              <i className="fa fa-times" />
+            </Button>
+          </OverlayTrigger>
+        </td>
+      </tr>,
+    );
+  }
+  return <tbody>{tasks}</tbody>;
+};
 
 export default Tasks;
