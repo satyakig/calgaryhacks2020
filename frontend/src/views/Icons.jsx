3c1d7f5aca76de6502723f25483a6e3fe1c20a7e
Mohit Parmar <mohitparmar9@outlook.com>
2020-02-15 21:04:32 -0700

    adjust calendar height

--- frontend/src/views/Icons.jsx
+++ frontend/src/views/Icons.jsx
@@ -46,6 +46,7 @@ export default () => {
         right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
       }}
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      contentHeight={800}
       // ref={this.calendarComponentRef}
       // weekends={this.state.calendarWeekends}
       // events={this.state.calendarEvents}
