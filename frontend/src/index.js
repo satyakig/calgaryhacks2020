134831853cb85a10c161ef1eab6e0ec3c2eb1ffe
Satyaki Ghosh <satyakig901@hotmail.com>
2020-02-25 23:46:11 -0700

    Removing unused files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,6 @@ import { createLogger } from 'redux-logger';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/animate.min.css';
 import './assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0';
-import './assets/css/demo.css';
 import './assets/css/pe-icon-7-stroke.css';
 
 import App from './components/App/App';
